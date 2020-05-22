cf4b923193ded87213adfa121c0220b0b49b0af9
KaeroDot <KaeroDot@contact.me.on.git.hub>
2020-05-22 15:54:57 +0200

    fix: permrep for large data

--- permrep.m
+++ permrep.m
@@ -17,16 +17,26 @@
 function pr = permrep(E);
         R = size(E, 1);
         C = size(E, 2);
+        P = R^C;
         if C > 19
+                % this method generates random possibilities:
+                warning('Switching to Monte Carlo Method!')
+                if R ~= 2
+                        % because I am lazy to do it generally:
+                        error('MCM works only for two rows!')
+                end
+                M = 1e3;
+                b = round(rand(M, C));
+                pr = b.*repmat(E(1,:), M, 1) + not(b).*repmat(E(2,:), M, 1);
                 % for 19 it takes ~11 seconds on iCore 7
                 % for 20 it takes ~23 seconds on iCore 7
                 % for 21 it takes ~49 seconds on iCore 7
-                disp('this will take a long time...')
-        end
-        P = R^C;
-        ids = dec2base(0:P-1, R) - '0' + 1;
-        pr = zeros(P, size(E, 2));
-        for i = 1:P
-                pr(i, :) = diag(E(ids(i, :), :))';
+        else
+                % this method generates all possibilities:
+                ids = dec2base(0:P-1, R) - '0' + 1;
+                pr = zeros(P, size(E, 2));
+                for i = 1:P
+                        pr(i, :) = diag(E(ids(i, :), :))';
+                end
         end
 end
